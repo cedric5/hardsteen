1ad333d9bf922efaf2f3b4e540dda7e45954654a
Cedric Remond <cedric.remond@fabriquartz.com>
2017-09-19 17:52:54 +0200

    day 2

--- lib/game.ex
+++ lib/game.ex
@@ -1,7 +1,15 @@
 defmodule Game do
   def create_deck do
-    hand = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]  
-    Enum.shuffle hand   
+    deck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]  
+    Enum.shuffle deck   
+  end
+
+  def attacker(game) do
+    Enum.at(game.turn, 0)
+  end
+
+  def defender(game) do
+    Enum.at(game.turn, 1)
   end
 
   def deal_initial(player) do
@@ -15,26 +23,42 @@ defmodule Game do
 
   def deal_card(player) do
     [head | tail] = player.deck
-    player = Map.put(player,:deck, tail)
-    player = Map.put(player,:hand, player.hand ++ [head])
+    player
+    |> Map.put(:deck, tail)
+    |> Map.put(:hand, player.hand ++ [head])
   end
 
   def do_turn(game) do
-      current_player = Enum.find game.players, fn player ->
-        player.id == game.turn
-      end
-      do_play game current_player
-    end
+    attacker = deal_card game.players[attacker(game)]    
+    card = IO.gets "which card do you want to play?" |> String.strip
+    {card,_} = Integer.parse card
+    
+    attacker = do_attack game.players[attacker(game)], card
+    defender = do_damage game.players[defender(game)], card
 
-  def do_play(game, current_player) do
-    card = IO.gets 'which card do you want to play?'
+    new_players = game.players
+    |> Keyword.replace(attacker(game), attacker)
+    |> Keyword.replace(defender(game), defender)
 
+    Map.put(game, :players, new_players)
+  end
+
+
+
+  def do_attack(player, card) do
+    new_hand = List.delete(player.hand, card)
+    Map.put(player, :hand, new_hand)
+  end
+
+  def do_damage(player, hit) do
+    new_health = player.health - hit
+    Map.put(player, :health, new_health)
   end
 
   def switch_turn(game) do
     case game.turn do
-      0 -> Map.put(game, :turn, 1)
-      1 -> Map.put(game, :turn, 0)
+      [:p1, :p2] -> Map.put(game, :turn, [:p2, :p1])
+      [:p2, :p1] -> Map.put(game, :turn, [:p1, :p2])
     end
   end
 end
--- lib/hardsteen.ex
+++ lib/hardsteen.ex
@@ -2,18 +2,21 @@ defmodule Hardsteen do
   
   def start do
       players = [
-      Game.deal_initial(%{hand: [], deck: Game.create_deck, id: 0, health: 30}),
-      Game.deal_initial(%{hand: [], deck: Game.create_deck, id: 1, health: 30}) 
+      p1: Game.deal_initial(%{hand: [], deck: Game.create_deck, id: 0, health: 30}),
+      p2: Game.deal_initial(%{hand: [], deck: Game.create_deck, id: 1, health: 30}) 
     ]
-    game = %{players: players, turn: 0, state: :playing}
-
+    game = %{players: players, turn: [:p1, :p2], state: :playing}
     game = do_turn game
-
-    IO.puts inspect game
   end
 
   def do_turn(game) do
-    game = game |> Game.do_turn |> Game.switch_turn
+    IO.puts "----------------"
+    IO.puts inspect game.turn
+    IO.puts "player_1: #{inspect game.players[:p1]}"
+    IO.puts "player_2: #{inspect game.players[:p2]}" 
+    IO.puts "----------------"  
+    game = Game.do_turn game
+    game = Game.switch_turn game
     case game.state do
       :playing -> do_turn game
       #:done   -> game.finish_game game
